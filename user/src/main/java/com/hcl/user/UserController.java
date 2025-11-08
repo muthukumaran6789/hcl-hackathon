eeb5cfe9df2411d489cd5c3c969e1327b89e9917
muthukumarn kannan <muthukumaran6789@gmail.com>
2025-11-08 13:05:26 +0530

    initial code

---
+++ user/src/main/java/com/hcl/user/UserController.java
@@ -0,0 +1,15 @@
+package com.hcl.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello, User!";
+    }
+}
